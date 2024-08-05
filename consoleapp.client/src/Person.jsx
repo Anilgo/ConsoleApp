02a36b541f80f8ee322383793324c8f80a543a6c
anilgo <46276209+Anilgo@users.noreply.github.com>
2024-08-05 16:16:08 +0300

    Added React-BootStrap to Project, Created temporary Person data and rendered on Screen

--- consoleapp.client/src/Person.jsx
+++ consoleapp.client/src/Person.jsx
@@ -1,35 +1,76 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import Table from 'react-bootstrap/Table';
 
 const Person = () => {
 
-    const personData =[
+    const personData = [
         {
-            id : 1,
+            id: 1,
             name: "Osman Yagmurdereli",
             email: "osman@osman.com",
-            phoneNumber : 511111111,
+            phoneNumber: 511111111,
             company: "Osman AS"
         },
         {
-            id : 2,
+            id: 2,
             name: "Adnan Senses",
             email: "adnan@adnan.com",
-            phoneNumber : 5222222222,
+            phoneNumber: 5222222222,
             company: "Adnan AS"
         },
         {
-            id : 1,
+            id: 1,
             name: "Onur Onuroglu",
             email: "onur@onur.com",
-            phoneNumber : 5333333333,
+            phoneNumber: 5333333333,
             company: "Osman AS"
         },
     ]
 
-    const [data,setData] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        setData(personData)
+    },
+        []
+    )
 
     return (
-        <div>Person</div>
+        <Fragment>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Company & Job Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data && data.length > 0 ?
+                            data.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{item.id}</td>
+                                        <td>{item.name}</td>
+                                        <td>{item.email}</td>
+                                        <td>{item.phoneNumber}</td>
+                                        <td>{item.company}</td>
+                                    </tr>
+
+                                )
+                            })
+                            :
+                            "Loading"
+                    }
+
+                </tbody>
+            </Table>
+        </Fragment>
     )
 }
 
